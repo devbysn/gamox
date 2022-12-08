642d914f5114b4b58d8f3715b0ac7de7a4b09903
Satyanarayan <satyanarayan.s@lntecc.com>
2022-12-08 10:27:24 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import AppStack from './src/navigation/AppStack';
+const App = () => {
+  return (
+    <NavigationContainer>
+      <AppStack />
+    </NavigationContainer>
+  );
+};
+
+export default App;
